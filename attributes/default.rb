924e862e8790d7bec30ed2a79c856765a92811e0
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-12 13:11:48 -0700

    use nscd user if we find it, nobody if not...
    this should make the change backcompat -- if there is an nscd user we'll
    use it, if not we won't.
    
    note that due to glibc caching (and since we're managing nscd) on first
    provisioning this may miss the creation of the nscd user and get into
    a chicken/egg problem.  the best solution there is not to bother
    creating an nscd user and to just use 'nobody'.

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,8 @@
 # limitations under the License.
 #
 
+require 'etc'
+
 # Possible values: nscd, unscd, gnscd
 default['nscd']['package'] = 'nscd'
 default['nscd']['version'] = nil
@@ -25,7 +27,7 @@ default['nscd']['version'] = nil
 default['nscd']['logfile'] = '/var/log/nscd'
 default['nscd']['threads'] = 4
 default['nscd']['max_threads'] = 32
-default['nscd']['server_user'] = 'nobody'
+default['nscd']['server_user'] = Etc.getpwent('nscd') ? 'nscd' : 'nobody'
 default['nscd']['stat_user'] = 'root'
 default['nscd']['debug_level'] = 0
 default['nscd']['reload_count'] = 5
