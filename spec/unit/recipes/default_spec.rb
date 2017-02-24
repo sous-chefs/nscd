79897793354bb367d1a060d91db7cea1dd4add54
Tim Smith <tsmith@chef.io>
2017-02-23 20:10:30 -0800

    Update specs to avoid deprecation warning
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'nscd::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
 
   it 'installs nscd' do
     expect(chef_run).to install_package('nscd')
