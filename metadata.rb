e7004ef391501399f7642930965e3aa202112274
Ethan Hampton <hamptone@osuosl.org>
2021-02-22 11:32:35 -0800

    Update metadata
    Signed-off-by: Ethan Hampton <hamptone@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,19 @@
-name 'nscd'
-maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
-license 'Apache-2.0'
-description 'Installs and configures nscd'
-version '5.1.0'
+name              'nscd'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs and configures nscd'
+version           '5.1.0'
+source_url        'https://github.com/sous-chefs/nscd'
+issues_url        'https://github.com/sous-chefs/nscd/issues'
+chef_version      '>= 12.15'
 
-%w(ubuntu debian fedora centos redhat oracle scientific amazon opensuseleap).each do |os|
-  supports os
-end
-
-source_url 'https://github.com/chef-cookbooks/nscd'
-issues_url 'https://github.com/chef-cookbooks/nscd/issues'
-chef_version '>= 12.15'
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'opensuseleap'
+supports 'oracle'
+supports 'redhat'
+supports 'scientific'
+supports 'ubuntu'
