b45bb3435098d7b2cbbc8d49b1a9c956c5b7aabc
Lamont Granquist <lamont@scriptkiddie.org>
2021-05-25 13:21:24 -0700

    Enable unified_mode and bump chef_version constraint
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- resources/clear_cache.rb
+++ resources/clear_cache.rb
@@ -2,7 +2,7 @@
 # Cookbook:: nscd
 # resource:: clear_cache
 #
-# Copyright:: 2015-2016, Chef Software, Inc.
+# Copyright:: Copyright (c) Chef Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+unified_mode true
 
 property :databases, Array, default: %w(passwd group hosts services netgroup)
 
